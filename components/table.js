bd84004c60e1f32b2f11d90b33ea5c5eb98422f8
devsamahd <devsamahd@gmail.com>
2023-02-24 08:21:57 +0100

    initial commit

---
+++ components/table.js
@@ -0,0 +1,46 @@
+import { TableContainer, Th, Td, Table, TableCaption, Thead, Tr, Tbody, Tfoot } from '@chakra-ui/react'
+import React from 'react'
+
+const CustomTable = ({tvalue}) => {
+    const used = tvalue.filter(t => t.used === true)
+    let amt = []
+    tvalue.map(t => amt.push(t.value))
+    let total=0;
+    for (let i = 0; i < amt.length; i++) {
+        total = total + amt[i]; 
+    }
+
+  return (
+    <TableContainer>
+        <Table variant='striped' colorScheme='teal'>
+            <TableCaption>All codes</TableCaption>
+            <Thead>
+            <Tr>
+                <Th>S/N</Th>
+                <Th>Code</Th>
+                <Th isNumeric>value</Th>
+                <Th>Status</Th>
+            </Tr>
+            </Thead>
+            <Tbody>
+            {tvalue.map((tv,key) => <Tr key={key}>
+                <Td>{key+1}</Td>
+                <Td>{tv.code}</Td>
+                <Td isNumeric>#{tv.value}</Td>
+                <Td color={tv.used ? 'red' : 'green'}>{tv.used ? 'used' : 'Valid'}</Td>
+            </Tr>)}
+            </Tbody>
+            <Tfoot>
+            <Tr>
+                <Th>Total: {tvalue.length}</Th>
+                <Th>Used: {used.length} Unused: {tvalue.length - used.length}</Th>
+                <Th isNumeric>Total: {total}</Th>
+                <Th ></Th>
+            </Tr>
+            </Tfoot>
+        </Table>
+    </TableContainer>
+  )
+}
+
+export default CustomTable

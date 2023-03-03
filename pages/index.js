f2f28316d1afe96e957a0683a13a25fd55223f24
devsamahd <devsamahd@gmail.com>
2023-03-04 00:45:00 +0100

    ...

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import { Center, Heading } from '@chakra-ui/react'
+import { Footer } from '@/components/Footer'
+import Hero from '@/components/hero'
+import Meta from '@/components/Meta'
+import { Nav } from '@/components/Nav'
+import { Stack } from '@chakra-ui/react'
 import React from 'react'
 
 const index = () => {
   return (
-    <Center>
-      <Heading>Awesome Landing page. Coming up! 🥳</Heading>
-    </Center>
+    <>
+    <Meta title={"MarketPro NG 💚"} />
+    <Nav />
+    <Stack bg={'#FFF7EB'}>
+    <Hero />
+    
+    </Stack>
+    <Footer />
+    </>
   )
 }
 
